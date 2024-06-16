0c3173b3e5a56ebf8abebb4f5d7cce0cc4d2d29a
kalimortis <85785193+Maximgg21@users.noreply.github.com>
2024-06-16 11:51:29 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const contactForm = document.getElementById("contact-form");
+const inputField = document.getElementsByClassName("input-field");
+const submitBtn = document.getElementById("submit-btn");
+
+contactForm.addEventListener("submit", function(e) {
+})
